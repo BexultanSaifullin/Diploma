913c41e2372de79c9eb7cb41e4c8ab849e168b5b
bexultan <beksulan15042003@icloud.com>
2024-02-03 22:20:39 +0600

    BF

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,13 +8,17 @@ public class CameraController : MonoBehaviour
     public float rotationSpeed = 2.0f;
     public float MaxY = 50.0f;
     public float MinY = 0.0f;
+    public CinemachineVirtualCamera VirtualCameras;
 
     void Update()
     {
         // Change height based on WASD input
-        float vertical = Input.GetAxis("Vertical");
-        float newY = Mathf.Clamp(transform.position.y + vertical * moveSpeed * Time.deltaTime, MinY, MaxY);
-        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        if (VirtualCameras.Priority == 1)
+        {
+            float vertical = Input.GetAxis("Vertical");
+            float newY = Mathf.Clamp(transform.position.y + vertical * moveSpeed * Time.deltaTime, MinY, MaxY);
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        }
 
         // Tilt camera downwards based on height
         float tiltAngle = Mathf.Clamp(transform.position.y* rotationSpeed, 0, maxTiltAngle);
