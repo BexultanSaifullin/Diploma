9853190c1c7c56fff08890241afc6a4e45e66b63
bexultan <beksulan15042003@icloud.com>
2024-02-04 16:40:44 +0600

    NF более адекватный ZOOM

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,9 +9,13 @@ public class CameraController : MonoBehaviour
     public float MaxY = 50.0f;
     public float MinY = 0.0f;
     public CinemachineVirtualCamera VirtualCameras;
-    float zSpeed = 5.0f;
-    public float maxZ = 20.0f;
-    public float minZ = 0.0f;
+    private float targetFieldOfView = 60;
+    private float targetFieldOfViewMin = 10;
+    private float targetFieldOfViewMax = 60;
+
+    //float zSpeed = 5.0f;
+    //public float maxZ = 20.0f;
+    //public float minZ = 0.0f;
 
     void Update()
     {
@@ -21,15 +25,35 @@ public class CameraController : MonoBehaviour
             float vertical = Input.GetAxis("Vertical");
             float newY = Mathf.Clamp(transform.position.y + vertical * moveSpeed * Time.deltaTime, MinY, MaxY);
             transform.position = new Vector3(transform.position.x, newY, transform.position.z);
-            float mouseScrollWheel = Input.GetAxis("Mouse ScrollWheel");
-            float newZCoordinate = transform.position.z + mouseScrollWheel * zSpeed;
-            newZCoordinate = Mathf.Clamp(newZCoordinate, minZ, maxZ);
-            transform.position = new Vector3(transform.position.x, transform.position.y, newZCoordinate);
+            HandleCameraZoom();
+
+            //float mousescrollwheel = input.getaxis("mouse scrollwheel");
+            //float newzcoordinate = transform.position.z + mousescrollwheel * zspeed;
+            //newzcoordinate = mathf.clamp(newzcoordinate, minz, maxz);
+            //transform.position = new vector3(transform.position.x, transform.position.y, newzcoordinate);
         }
 
         // Tilt camera downwards based on height
         float tiltAngle = Mathf.Clamp(transform.position.y* rotationSpeed, 0, maxTiltAngle);
         transform.localEulerAngles = new Vector3(tiltAngle, 0, 0);
 
+    }
+    private void HandleCameraZoom()
+    {
+        float fieldOfViewIncreaseAmount = 5f;
+        if(Input.mouseScrollDelta.y > 0)
+        {
+            targetFieldOfView += fieldOfViewIncreaseAmount;
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            targetFieldOfView -= fieldOfViewIncreaseAmount;
+        }
+        targetFieldOfView = Mathf.Clamp(targetFieldOfView, targetFieldOfViewMin, targetFieldOfViewMax);
+
+
+        float zoomSpeed = 5f;
+        Mathf.Lerp(VirtualCameras.m_Lens.FieldOfView, targetFieldOfView, Time.deltaTime * zoomSpeed);
+        VirtualCameras.m_Lens.FieldOfView = targetFieldOfView;
     }
 }
