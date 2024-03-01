9a88e535a529163dbf6db27823d26e857e90d93a
bexultan <beksulan15042003@icloud.com>
2024-03-01 17:23:46 +0600

    [NF] Бот

--- Assets/Scripts/Managers/GameManagerScr.cs
+++ Assets/Scripts/Managers/GameManagerScr.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
 
 
 
@@ -47,8 +48,72 @@ public class GameManagerScr : MonoBehaviour
                 TurnTimeTxt.text = TurnTime.ToString();
                 yield return new WaitForSeconds(1);
             }
+            
+            List<GameObject> EnemyPlaces;
+            
+            GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag("free");
+
+            
+            int EnemyLayer = LayerMask.NameToLayer("EnemyPlace");
+            GameObject[] objectsOnLayer = objectsWithTag.Where(card => card.layer == EnemyLayer).ToArray();
+
+
+            EnemyPlaces = objectsOnLayer.ToList();
+            List<GameObject> EnemyCard;
+
+            GameObject[] objectsWithTagE = GameObject.FindGameObjectsWithTag("EnemyCard");
+
+
+            int EnemyLayerE = LayerMask.NameToLayer("Enemy");
+            GameObject[] objectsOnLayerE = objectsWithTagE.Where(card => card.layer == EnemyLayerE).ToArray();
+
+
+            EnemyCard = objectsOnLayerE.ToList();
+            //List<GameObject> EnemyCard = GameObject.FindGameObjectsWithTag("EnemyCard").ToList();
+
+            if (EnemyCard.Count > 0 && EnemyPlaces.Count > 0)
+            {
+                EnemyTurn(EnemyCard, EnemyPlaces);    
+            }
         }
         ChangeTurn();
+    }
+
+    void EnemyTurn(List<GameObject> EnemyCard, List<GameObject> EnemyPlaces)
+    {
+        if(EnemyPlaces.Count > EnemyCard.Count)
+        {
+            int count = Random.Range(-1, EnemyCard.Count);
+            Debug.Log(count);
+            if (count == -1)
+                    return;
+          
+            for (int i = count; i >= 0; i--)
+            {
+                int place = Random.Range(0, EnemyPlaces.Count-1);
+                EnemyCard[i].transform.position = EnemyPlaces[place].transform.position;
+                EnemyCard[i].layer = LayerMask.NameToLayer("EnemyPlaying");
+                EnemyPlaces[place].gameObject.tag = "busy";
+                EnemyPlaces.RemoveAt(place);
+                EnemyCard.RemoveAt(i);
+            }
+        } else
+        {
+            int count = Random.Range(-1, EnemyPlaces.Count);
+            Debug.Log(count);
+            if (count == -1)
+                return;
+            for (int i = count; i >= 0; i--)
+            {
+                int place = Random.Range(0, EnemyPlaces.Count-1);
+                EnemyCard[i].transform.position = EnemyPlaces[place].transform.position;
+                EnemyCard[i].layer = LayerMask.NameToLayer("EnemyPlaying");
+                EnemyPlaces[place].gameObject.tag = "busy";
+                EnemyPlaces.RemoveAt(place);
+                EnemyCard.RemoveAt(i);
+            }
+        }
+
     }
     public void ChangeTurn()
     {
