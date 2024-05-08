571c605a8b760720e884a3f2ccfcfe59d9a43ebb
Darkhan <darhantashim@gmail.com>
2024-05-09 01:54:34 +0500

    [NF] добавил забор в префаб без логики и поменял характеристики карт

--- Assets/Scripts/Managers/CardManager.cs
+++ Assets/Scripts/Managers/CardManager.cs
@@ -51,16 +51,16 @@ public class CardManager : MonoBehaviour
     public GameObject[] cardModels;
     public void Awake()
     {
-        CardManagerList.AllCards.Add(new Card("Warrior", 1, 10, 1, 3, "Unit", false, 1));
-        CardManagerList.AllCards.Add(new Card("Yurt", 0, 10, 0, 2, "Building", false, 2));
-        CardManagerList.AllCards.Add(new Card("Barak", 0, 10, 0, 2, "Building", false, 3));
-        CardManagerList.AllCards.Add(new Card("Bowrange", 0, 10, 0, 2, "Building", false, 4));
+        CardManagerList.AllCards.Add(new Card("Warrior", 2, 4, 1, 3, "Unit", false, 1));
+        CardManagerList.AllCards.Add(new Card("Yurt", 0, 4, 0, 2, "Building", false, 2));
+        CardManagerList.AllCards.Add(new Card("Barak", 0, 4, 0, 4, "Building", false, 3));
+        CardManagerList.AllCards.Add(new Card("Bowrange", 0, 5, 0, 4, "Building", false, 4));
         CardManagerList.AllCards.Add(new Card("Man", 1, 1, 1, 1, "Unit", false, 5));
-        CardManagerList.AllCards.Add(new Card("Archer", 1, 10, 2, 4, "Unit", false, 6));
+        CardManagerList.AllCards.Add(new Card("Archer", 3, 2, 2, 3, "Unit", false, 6));
         CardManagerList.AllCards.Add(new Card("Ensign", 1, 5, 1, 4, "Unit", true, 7));
         CardManagerList.AllCards.Add(new Card("Mystan", 1, 1, 1, 4, "Unit", true, 8));
-        CardManagerList.AllCards.Add(new Card("Arrows", 1, 0, 1, 2, "Spell", false, 9));
-        CardManagerList.AllCards.Add(new Card("Jut", 2, 0, 0, 1, "Spell", false, 10));
+        CardManagerList.AllCards.Add(new Card("Arrows", 1, 0, 1, 1, "Spell", false, 9));
+        CardManagerList.AllCards.Add(new Card("Jut", 2, 0, 0, 2, "Spell", false, 10));
 
 
 
