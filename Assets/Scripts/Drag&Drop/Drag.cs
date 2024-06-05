7f28bc01f307edb1f9a973df3191dae0d481e2f9
bexultan <beksultan15042003@icloud.com>
2024-06-06 03:10:19 +0500

    [BF] мелочь

--- Assets/Scripts/Drag&Drop/Drag.cs
+++ Assets/Scripts/Drag&Drop/Drag.cs
@@ -285,6 +285,7 @@ public class Drag : InformationManagerScr
             base.ShowManaPlayer();
             GameManager.ShowHPWall();
             DestroyImmediate(selectedObject);
+            ArrangeCards();
         }
     }
 
