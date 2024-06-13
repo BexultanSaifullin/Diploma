ce47422623d5b66d58269574374a2a7690c9a4a2
bexultan <beksultan15042003@icloud.com>
2024-06-13 12:05:26 +0500

    [NF] Гуардианы

--- Assets/Scripts/Cards/CardInfoScr.cs
+++ Assets/Scripts/Cards/CardInfoScr.cs
@@ -57,7 +57,7 @@ public class CardInfoScr : MonoBehaviour
         // {
         //     randomNumber = 8;
         // }
-        ShowCardInfo(CardManagerList.AllCards[5], 5);
+        ShowCardInfo(CardManagerList.AllCards[randomNumber], randomNumber);
     }
 
     public void NotRandom(int randomNumber)
